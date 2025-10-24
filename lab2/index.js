934772eb563e00b0e27746c374c81dc937034d40
Julia ANS <say.julia.vi@gmail.com>
2025-10-24 16:59:14 +0200

    lab2 finish

--- lab2/index.js
+++ lab2/index.js
@@ -13,10 +13,44 @@
 // console.log(data.toString());
 // console.log("Koniec skryptu.");
 
-const fs = require("fs");
-console.log("Początek skryptu.");
-fs.readFile('package.json', function (err, data) {
- if (err) return console.error(err);
- console.log(data.toString());
+// const fs = require("fs");
+// console.log("Początek skryptu.");
+// fs.readFile('package.json', function (err, data) {
+//  if (err) return console.error(err);
+//  console.log(data.toString());
+// });
+// console.log("Koniec skryptu.");
+
+
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 8888;
+// const server = http.createServer(function(request, response) {
+//  response.statusCode = 200;
+//  response.setHeader('Content-Type', 'text/plain');
+//  response.end('Witaj, z serwera node.js !\n');
+
+// });
+// server.listen(port, hostname, () => {
+//  console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const http = require('http');
+const path = require("path");
+const v8 = require("v8");
+
+const hostname = '127.0.0.1';
+const port = 8888;
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello, from Node.js server!\n');
 });
-console.log("Koniec skryptu.");
+
+server.listen(port, hostname, () => {
+  console.log(`[${new Date().toISOString()}] Heap Statistics:`, v8.getHeapStatistics());
+  console.log("Current file:", path.basename(__filename));
+  console.log("Uploads path:", path.join(__dirname, 'uploads', 'images'));
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
